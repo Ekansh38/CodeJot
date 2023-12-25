db5576d1584d55a59669e54009e023a82f928b60
Ekansh Goenka <goenka.ekansh@gmail.com>
2023-12-25 23:52:23 +0800

    added views and templates

--- CodeJot/notes/urls.py
+++ CodeJot/notes/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
-    path('addNote/<str:note>', views.add_note, name='addNote'),
+    path('addNote', views.add_note, name='addNote'),
 ]
--- CodeJot/notes/views.py
+++ CodeJot/notes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    pass
+
+def login(request):
+    pass
+
+def signup(request):
+    pass
+
+def logout(request):
+    pass
+
+def add_note(request):
+    pass
