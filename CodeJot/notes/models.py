961844e6ce4df85697e11f3b07b218f86fc1fd23
Ekansh Goenka <goenka.ekansh@gmail.com>
2023-12-26 20:19:10 +0800

    added basic auth

--- CodeJot/notes/models.py
+++ CodeJot/notes/models.py
@@ -4,3 +4,9 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+
+class Note(models.Model):
+    id = models.AutoField(primary_key=True)
+    note_data = models.TextField()
+    notes_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
--- CodeJot/notes/views.py
+++ CodeJot/notes/views.py
@@ -1,18 +1,75 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .models import User, Note
 
 # Create your views here.
 
 def index(request):
-    pass
+    if request.user.is_authenticated:
+        notes = Note.objects.filter(notes_user=request.user)
+        return render(request, "notes/index.html", {
+            "notes": notes,
+        })
+
+    # else
+    return HttpResponseRedirect(reverse("login"))
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "notes/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    
+    # else
+        
+    return render(request, "notes/login.html")
+
 
 def signup(request):
-    pass
+    if request.method == "POST":
+        print('posted')
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password_confirm = request.POST["password_confirm"]
+        
+        if password != password_confirm:
+            return render(request, "notes/signup.html", {
+                "message": "Passwords must match."
+            })
+
+        # else
+        if User.objects.filter(username=username).exists():
+            return render(request, "notes/signup.html", {
+                "message": "Username already taken."
+                })
+            
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        login(request, user)
+        
+        return HttpResponseRedirect(reverse("index"))
+
+        
+    # else
+    return render(request, "notes/signup.html")
+
 
 def logout(request):
-    pass
+    logout(request)
 
 def add_note(request):
     pass
